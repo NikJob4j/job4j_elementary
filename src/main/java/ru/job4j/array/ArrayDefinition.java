66af5c1eaba10aa27835721ec776e38c4870e99a
Niki2603 <Soulmusik@yandex.ru>
2022-09-08 17:14:29 +0300

    6.0.2. Размер массива [#333582 #313481]

---
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        System.out.println("Размер масива равен:" + ages.length);
+        String[] surnames = new String[100500];
+        System.out.println("Размер массива равен:" + surnames.length);
+        float[] prices = new float[40];
+        System.out.println("Размер массива равен:" + prices.length);
+        String[] names = new String[4];
+        names[0] = "Nikita";
+        names[1] = "Dasha";
+        names[2] = "Dad";
+        names[3] = "Mom";
+        String name = names[0];
+        System.out.println(name);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+    }
+}
